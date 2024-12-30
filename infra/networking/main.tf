489852cb023613ce354bb8a050546f79906b2bfa
vasuchallapu <vasuchallapu@gmail.com>
2024-12-30 14:17:25 +0530

    fixing typo issue availability zone

--- infra/networking/main.tf
+++ infra/networking/main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "dev_proj_1_public_subnets" {
   count             = length(var.cidr_public_subnet)
   vpc_id            = aws_vpc.dev_proj_1_vpc_us_east_1.id
   cidr_block        = element(var.cidr_public_subnet, count.index)
-  availability_zone = element(var.eu_availability_zone, count.index)
+  availability_zone = element(var.us_availability_zone, count.index)
 
   tags = {
     Name = "dev-proj-public-subnet-${count.index + 1}"
